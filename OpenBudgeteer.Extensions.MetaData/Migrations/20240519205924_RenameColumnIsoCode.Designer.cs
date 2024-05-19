22fdf5d3d8b6c68c392b49dd1028d44dca8774ba
Anu6is <curtis.mayers@gmail.com>
2024-05-19 19:41:14 -0400

    [migration] rename column ISO_Code -> IsoCode

--- OpenBudgeteer.Extensions.MetaData/Migrations/20240519205924_RenameColumnIsoCode.Designer.cs
+++ OpenBudgeteer.Extensions.MetaData/Migrations/20240519205924_RenameColumnIsoCode.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpenBudgeteer.Extensions.MetaData;
 
@@ -10,9 +11,11 @@ using OpenBudgeteer.Extensions.MetaData;
 namespace OpenBudgeteer.Extensions.MetaData.Migrations
 {
     [DbContext(typeof(ExtendedDatabaseContext))]
-    partial class ExtendedDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240519205924_RenameColumnIsoCode")]
+    partial class RenameColumnIsoCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
---
+++ OpenBudgeteer.Extensions.MetaData/Migrations/20240519205924_RenameColumnIsoCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenBudgeteer.Extensions.MetaData.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnIsoCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Currency_ISO_Code",
+                table: "AccountDetail",
+                newName: "Currency_IsoCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Currency_IsoCode",
+                table: "AccountDetail",
+                newName: "Currency_ISO_Code");
+        }
+    }
+}
